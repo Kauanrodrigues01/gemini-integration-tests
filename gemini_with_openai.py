745686166fdcd533289eb65260f8c4f89d63e15e
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-01-08 07:29:49 -0300

    using gemini with openai library

---
+++ gemini_with_openai.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+from decouple import config
+
+API_KEY = config('API_KEY', cast=str)
+
+client = OpenAI(
+    api_key=API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model="gemini-1.5-flash",
+    n=1,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
